a8a5ab26dedbb26466db9064faa227c97b6b9baf
nandanchakraborty <nandanchakraborty90@gmail.com>
2023-11-04 21:38:25 +0600

    new code

---
+++ codeforces_1348A.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int t;
+
+cin>>t;
+
+while(t--){
+int n;
+cin>>n;
+int ar[n];
+if(n==2) cout<<2<<endl;
+else {
+for(int i=2;i<=n*2;i+=2)
+{
+
+
+}
+}
+
+}
+
+}
