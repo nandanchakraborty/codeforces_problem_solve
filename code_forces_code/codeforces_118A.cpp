af86147a3cc2b5ab509540d15c5925d0ab01bf29
nandanchakraborty <nandanchakraborty90@gmail.com>
2023-11-07 20:32:12 +0600

    string task

---
+++ code_forces_code/codeforces_118A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+{
+string s;
+cin>>s;
+transform(s.begin(),s.end(),s.begin(),::tolower);
+int a=0;
+while(a<s.length())
+{
+if(s[a]=='a' || s[a]=='e' || s[a]=='i' || s[a]=='o' || s[a]=='u' || s[a]=='y');
+else cout<<"."<<s[a];
+a++;
+}
+cout<<endl;
+
+}
+}
