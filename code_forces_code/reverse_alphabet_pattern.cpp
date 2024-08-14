f9548e204b4c1f8de0ae49a5b2108c93a0e4eacc
nandanchakraborty <nandancsebubt@gmail.com>
2024-08-14 23:01:36 +0600

    pattern problem

---
+++ code_forces_code/reverse_alphabet_pattern.cpp
@@ -0,0 +1,20 @@
+
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main() {
+    int n,i,j;
+    cin>>n;
+    char p='A';
+    for(i=0; i<n; i++) {
+
+          for(char ch =(p+n-1)-i;ch<=(p+n-1);ch++){
+
+           cout<<ch<<" ";
+        }
+
+
+
+        cout<<endl;
+        }
+        }
