142dc004a8055d12a66176ead461adce7437736b
nandanchakraborty <nandancsebubt@gmail.com>
2024-08-17 20:03:49 +0600

    codeforces contest

---
+++ code_forces_code/codeforces_education_contest_round_164.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--) {
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a==b && b==c) cout<<"NO"<<endl;
+        else if(a<b) cout<<"NO"<<endl;
+        else if(b==c) cout<<"NO"<<endl;
+        else if(b>c) cout<<"YES"<<endl;
+
+
+
+
+
+
+
+    }
+
+}
